d35cf2df40bb64bd69a620e37bab9a4a4a05c82a
Payne <2643760427@qq.com>
2026-03-19 18:29:19 +0800

    Layout静态页面的搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,16 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
   <div>
+    <LayoutNav></LayoutNav>
+    <LayoutHeader></LayoutHeader>
     MainPage
     <!-- 二级路由出口 -->
     <RouterView></RouterView>
+    <LayoutFooter></LayoutFooter>
   </div>
 </template>
