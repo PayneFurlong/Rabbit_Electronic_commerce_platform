067b4bcadb69dbf16d4e5cfbcd983248192e9aae
Payne <2643760427@qq.com>
2026-03-20 18:54:51 +0800

    home面板组件封装

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 <template>
   <!-- 一级路由出口 -->
   <RouterView></RouterView>
-  <div class="test">123456</div>
 </template>
 
 <style scoped lang="scss">
--- src/apis/home.js
+++ src/apis/home.js
@@ -1,3 +1,4 @@
 import http from '@/utils/http'
 
+// 获取 banner 轮播图
 export const getBannerAPI = () => http.get('/home/banner')
