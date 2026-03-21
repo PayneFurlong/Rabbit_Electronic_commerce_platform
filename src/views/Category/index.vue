27b39f87917fc77c59e04926ea9906e2291f36cd
Payne <2643760427@qq.com>
2026-03-22 00:04:25 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,19 +4,27 @@ import { getBannerAPI } from '@/apis/home'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import GoodsItem from '../Home/components/GoodsItem.vue'
+import { onBeforeRouteUpdate } from 'vue-router'
 
 // 数据容器
 const categoryData = ref({})
 // 获取 route 实例对象
 const route = useRoute()
 // 获取二级分类列表
-const getCategory = async () => {
+const getCategory = async (id = route.params.id) => {
   // 发送请求
-  const res = await getCategoryAPI(route.params.id)
+  const res = await getCategoryAPI(id)
   // 存入容器
   categoryData.value = res.result
 }
 getCategory()
+
+// 目标：路由参数变化的时候，可以把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+  // 存在问题是：使用最新的路由参数请求最新的分类数据
+  // 重新渲染二级分类列表
+  getCategory(to.params.id)
+})
 
 // banner 模块
 // banner 列表容器
