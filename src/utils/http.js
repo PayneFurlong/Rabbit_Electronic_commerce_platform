c93fea3faa4a8cc5cc483988f0b9a3e65012c7eb
Payne <2643760427@qq.com>
2026-03-24 19:03:02 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 // 创建axios实例
 const http = axios.create({
@@ -30,8 +31,18 @@ http.interceptors.request.use(
 http.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    // 获取用户信息仓库
+    const userStore = useUserStore()
     // 统一错误提示
     ElMessage.warning(e.response.data.message)
+    // 401 token 失效处理
+    // 1. 清除本地数据
+    if (e.response.status === 401) {
+      // 清除本地用户数据
+      userStore.clearUserInfo()
+    }
+    // 2. 跳转至登录页
+    router.replace('/login')
     return Promise.reject(e)
   }
 )
