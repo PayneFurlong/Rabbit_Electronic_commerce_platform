5113dd1d526bfac7b6dad727427b8cb979dc3ed4
Payne <2643760427@qq.com>
2026-03-24 18:00:00 +0800

    登录-Pinia的数据持久化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import piniaPlugin from 'pinia-plugin-persistedstate'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
@@ -14,7 +15,10 @@ import { componentsPlugin } from '@/components/index'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPlugin)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,19 +2,25 @@ import { getLoginAPI } from '@/apis/user'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-  // 定义管理用户的 state
-  const userInfo = ref({})
-  // 定义获取接口数据的 action 函数
-  const getUserInfo = async ({ account, password }) => {
-    // 请求 user 信息
-    const res = await getLoginAPI({ account, password })
-    // 将数据存入 userInfo 中
-    userInfo.value = res.result
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    // 定义管理用户的 state
+    const userInfo = ref({})
+    // 定义获取接口数据的 action 函数
+    const getUserInfo = async ({ account, password }) => {
+      // 请求 user 信息
+      const res = await getLoginAPI({ account, password })
+      // 将数据存入 userInfo 中
+      userInfo.value = res.result
+    }
+    // 以对象的形式将 state 和 action 返回出去
+    return {
+      userInfo,
+      getUserInfo
+    }
+  },
+  {
+    persist: true
   }
-  // 以对象的形式将 state 和 action 返回出去
-  return {
-    userInfo,
-    getUserInfo
-  }
-})
+)
